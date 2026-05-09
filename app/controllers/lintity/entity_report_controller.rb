df94dd63c9951aa501c8310f373d804815acde10
Rem <r3mnik@gmail.com>
2026-05-09 22:19:20 +0300

    Add advanced styling

--- app/controllers/lintity/entity_report_controller.rb
+++ app/controllers/lintity/entity_report_controller.rb
@@ -2,6 +2,7 @@
 
 module Lintity
   class EntityReportController < ApplicationController
+    before_action :init_fields, only: [:index]
     # Child controllers inheriting from this controller should set @records
     # to an array of hashes with the structure described in the view.
     def index
